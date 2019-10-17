e766cc6f830be3a80d5d4b59f3e53d828afe7d3d
tsudanori <45957907+tsudanori@users.noreply.github.com>
2019-10-17 14:07:20 +0900

    プロジェクト

---
+++ fun medical/SettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingViewController.swift
+//  fun medical
+//
+//  Created by 津田忠範 on 2019/10/17.
+//  Copyright © 2019 fun medecal project. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
